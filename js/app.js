9648ce5146cc2c893699b9edc0f25884cfbe5007
Tom <mail@tomsisman.tech>
2018-07-17 17:14:19 +0100

    main-content-complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 $(document).foundation()
+
+
+$(document).ready(function () {
+    $('.product-categories').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                }
+            }
+        ]
+    });
+});
+
+
